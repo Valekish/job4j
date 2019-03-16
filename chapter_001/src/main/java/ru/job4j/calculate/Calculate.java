496a638942c362acd0b32ab184a88dd1d4f48581
Valekish <valekishwork@mail.ru>
2019-03-16 21:58:49 +0200

    1.3. JavaDoc. Code convention[#121449]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate
+ * Выводит строку "Hello world!" в консоль
+ * @author Valentin Kirjan (valekishwork@mail.ru)
+ */
+
 public class Calculate {
+	
+	/**
+	 * Main
+	 * @param args - args
+	 */
 	
 	public static void main (String[] args) {
 		System.out.println("Hello World");
