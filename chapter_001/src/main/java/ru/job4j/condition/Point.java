64b09923d5987ecd23f2e893d006776f23a2f911
Valekish <valekishwork@mail.ru>
2019-03-18 12:47:38 +0200

    Исправление 3.3. Идеальный вес.[#121436] и 3.4. Расстояние между точками в системе координат[#121440]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,6 +1,20 @@
 package ru.job4j.condition;
 
+/**
+ * Point.
+ * Вычисляет расстояние между точками в координатной плоскости.
+ * @author Valentin Kirjan (valekishwork@mail.ru).
+ */
 public class Point {
+
+    /**
+     * Distance.
+     * @param x1 - x1.
+     * @param y1 - y1.
+     * @param x2 - x2.
+     * @param y2 - y2.
+     * @return расстояние между x и y.
+     */
     public double distance(int x1, int y1, int x2, int y2) {
         double first = Math.pow(x2 - x1, 2);
         double second = Math.pow(y2 - y1, 2);
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -4,7 +4,17 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Test
+ * @author Valentin Kirjan (valekishwork@mail.ru).
+ * @version $Id$.
+ * @since 18.03.19.
+ */
 public class PointTest {
+
+    /**
+     * Test formula.
+     */
     @Test
     public void whenZeroAndTenThenTen() {
         Point point = new Point();
