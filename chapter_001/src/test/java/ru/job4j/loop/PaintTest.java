2ef7eecbdb1990c113233ef0c1ae4321fa4f6c04
Valekish <valekishwork@mail.ru>
2019-03-20 00:51:58 +0200

    5.4. Построить пирамиду в псевдографике.[#121420]

---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,75 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import java.util.StringJoiner;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Test.
+ * @author Valentin Kirjan (valekishwork@mail.ru).
+ * @version 0.1
+ * @since 19.03.19
+ */
+public class PaintTest {
+
+    /**
+     * Тест правой части.
+     */
+    @Test
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    /**
+     * Тест левой части.
+     */
+    @Test
+    public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    /**
+     * Тест пирамиды.
+     */
+    @Test
+    public void whenPyramid4LeftAnd4Right() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^   ")
+                                .add("  ^^^  ")
+                                .add(" ^^^^^ ")
+                                .add("^^^^^^^")
+                                .toString()
+                )
+        );
+    }
+}
